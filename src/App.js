93c9a8b088d8478e92875a0254ec9e343cb3a37a
Sarasabbagh <42205895+Sarasabbagh@users.noreply.github.com>
2021-12-09 13:42:00 +0000

    added github link into footer

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ function App() {
       <header className="App-header">
         <h1>Hello from React</h1>
         <Weather city="London" />
+        <p>
+          Coded by Sara Sabbagh with open sourced code on my{" "}
+          <a href="https://github.com/Saraesabbagh/React-Weather">github </a>{" "}
+          and hosted on netlify
+        </p>
       </header>
     </div>
   );
